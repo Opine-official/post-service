11db11a3779d499d55ded297ca783bfa0b51fba0
Aravind Sanjeev <saravind436@gmail.com>
2024-01-02 15:25:51 +0530

    Add cors package and configure CORS options

--- src/presentation/Server.ts
+++ src/presentation/Server.ts
@@ -1,12 +1,22 @@
 import express from 'express';
+import cors from 'cors';
 
-interface ServerControllers {}
+// interface ServerControllers {}
+
+const corsOptions = {
+  origin: 'https://localhost:3000',
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
 export class Server {
   public static async run(
     port: number,
-    controllers: ServerControllers,
+    // controllers: ServerControllers,
   ): Promise<void> {
     const app = express();
+    app.use(cors(corsOptions));
+    app.options('*', cors(corsOptions));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
 
